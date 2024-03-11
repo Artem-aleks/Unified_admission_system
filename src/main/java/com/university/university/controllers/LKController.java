1a96e3b95cb4f315a467836dd850b80d2d1811d2
Artem-aleks <aleksanova.artem2004@gmail.com>
2024-03-11 23:56:28 +0300

    Readme

--- src/main/java/com/university/university/controllers/LKController.java
+++ src/main/java/com/university/university/controllers/LKController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 
 @Controller
- public class LKController {
+public class LKController {
     @GetMapping("/LkMain")
     public String LKMain(Model model) {
         return "LkMain";
