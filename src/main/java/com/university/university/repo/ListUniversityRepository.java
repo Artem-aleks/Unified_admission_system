acb6cb333006530fdc27fe023fde9b2d4d8b928b
Artem-aleks <71816794+Artem-aleks@users.noreply.github.com>
2024-05-23 15:54:00 +0300

    Delete src/main/java/com/university/university directory

--- src/main/java/com/university/university/repo/ListUniversityRepository.java
+++
@@ -1,6 +0,0 @@
-package com.university.university.repo;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ListUniversityRepository extends CrudRepository<ListUniversityRepository, Long> {
-}
